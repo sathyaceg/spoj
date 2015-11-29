a1c920b1857250141d0f7058352e7d1e31c3e7ac
sathyaceg <kash.myl93@gmail.com>
2015-11-29 12:08:22 +0530

    Update spojsuperMario.cpp

--- spojsuperMario.cpp
+++ spojsuperMario.cpp
@@ -1,1 +1,1 @@
-If you're here to see the solution of a problem which is give in a live contest, say hell yeah and fuck off! 
+If you're here to see the solution of a problem which is given in a live contest, say hell yeah and fuck off! 
